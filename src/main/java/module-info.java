d4f1642771e1c97452943ea059ee498c1a6d12ec
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-12-17 16:12:17 +0430

    add h2 dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,17 @@ module net.golbarg.kankor.kankor {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.sql;
+    requires javafx.swing;
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires com.h2database;
+
     opens net.golbarg.kankor.kankor to javafx.fxml;
     exports net.golbarg.kankor.kankor;
+    exports net.golbarg.kankor.kankor.controller;
+    opens net.golbarg.kankor.kankor.controller to javafx.fxml;
 }

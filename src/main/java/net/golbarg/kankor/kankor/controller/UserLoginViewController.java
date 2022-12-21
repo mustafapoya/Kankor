9a5560a79250c3a8abac6eed3eb380944a671747
MustafaPoya <mustafapoyaabc@gmail.com>
2022-12-21 23:56:12 +0430

    create login form

---
+++ src/main/java/net/golbarg/kankor/kankor/controller/UserLoginViewController.java
@@ -0,0 +1,32 @@
+package net.golbarg.kankor.kankor.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserLoginViewController implements Initializable {
+
+    @FXML
+    private ImageView imgUserImage;
+    @FXML
+    private ComboBox comboUsername;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Button btnRegister;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        btnLogin.setOnAction(actionEvent -> {
+            System.out.println("trying to login");
+        });
+    }
+}

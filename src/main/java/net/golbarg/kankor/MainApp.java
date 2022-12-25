9ccce1603207046ce811c9f4c4f24194c48754cc
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-12-25 11:56:41 +0430

    create main view

--- src/main/java/net/golbarg/kankor/MainApp.java
+++ src/main/java/net/golbarg/kankor/MainApp.java
@@ -3,6 +3,7 @@ package net.golbarg.kankor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,14 +11,18 @@ import java.io.IOException;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("view/user-register-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        loadMainView(stage);
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private static void loadMainView(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("view/main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Kankor");
+        stage.setScene(scene);
+        stage.show();
     }
 }

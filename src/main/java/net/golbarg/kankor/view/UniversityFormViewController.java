a07e070c9813f918ecec560526553d81336f5b0a
MustafaPoya <mustafapoyaabc@gmail.com>
2023-01-10 23:29:37 +0430

    created university form layout

---
+++ src/main/java/net/golbarg/kankor/view/UniversityFormViewController.java
@@ -0,0 +1,51 @@
+package net.golbarg.kankor.view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UniversityFormViewController implements Initializable {
+    @FXML
+    private BorderPane root;
+    @FXML
+    private ComboBox<String> comboUniversity;
+    @FXML
+    private TextField txtSearch;
+    @FXML
+    private Button btnSearch;
+    @FXML
+    private TableView<String> tableUniversity;
+    @FXML
+    private TableColumn columnUniversity;
+    @FXML
+    private TableColumn columnFaculty;
+    @FXML
+    private TableColumn columnDepartment;
+    @FXML
+    private TableColumn columnCodeNumber;
+    @FXML
+    private TableColumn columnAdmission;
+
+    @FXML
+    private Accordion accordionChoice;
+    @FXML
+    private TitledPane titledPaneChoice1;
+    @FXML
+    private TitledPane titledPaneChoice2;
+    @FXML
+    private TitledPane titledPaneChoice3;
+    @FXML
+    private TitledPane titledPaneChoice4;
+    @FXML
+    private TitledPane titledPaneChoice5;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

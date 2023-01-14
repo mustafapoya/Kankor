df9f992932fdb999f9508773e4ad8bcdedc72a2a
Mustafa Poya <mustafapoyaabc@gmail.com>
2023-01-14 16:05:01 +0430

    create view files

---
+++ src/main/java/net/golbarg/kankor/model/Subject.java
@@ -0,0 +1,46 @@
+package net.golbarg.kankor.model;
+
+public class Subject {
+    private int id;
+    private String name;
+    private int score;
+
+    public Subject(int id, String name, int score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}

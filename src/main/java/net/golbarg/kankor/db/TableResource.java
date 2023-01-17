e68e2151a36a8b96d3e548bc24744d3e1e5be0a6
Mustafa Poya <mustafapoyaabc@gmail.com>
2023-01-17 10:35:39 +0430

    Create resource and resource category tables

---
+++ src/main/java/net/golbarg/kankor/db/TableResource.java
@@ -0,0 +1,177 @@
+package net.golbarg.kankor.db;
+
+import net.golbarg.kankor.model.Question;
+import net.golbarg.kankor.model.Resource;
+import net.golbarg.kankor.model.ResourceCategory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class TableResource implements CRUDHandler<Resource> {
+    public static final String TABLE_NAME = "EMAILS";
+    public static final String [] COLUMNS = {"ID", "CATEGORY_ID", "NAME", "FILE_NAME", "DESCRIPTION", "SIZE"};
+    public static final String COLUMNS_STR = "ID, CATEGORY_ID, NAME, FILE_NAME, DESCRIPTION, `SIZE`";
+
+
+    @Override
+    public boolean create(Resource object) {
+        String query = String.format("insert into %s (CATEGORY_ID, NAME, FILE_NAME, DESCRIPTION, `SIZE`) values (?, ?, ?, ?, ?)", TABLE_NAME);
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement = putValues(statement, object);
+            statement.executeUpdate();
+
+            return true;
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public Resource findById(int id) {
+        String query = String.format("SELECT %s FROM %s where id = ?;", COLUMNS_STR, TABLE_NAME);
+
+        Resource object = null;
+
+        try {
+
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if(resultSet.next()) {
+                object = mapColumn(resultSet);
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return object;
+    }
+
+    @Override
+    public ArrayList<Resource> getAll() {
+        String query = String.format("SELECT %s FROM %s;", COLUMNS_STR, TABLE_NAME);
+
+        ArrayList<Resource> resultList = new ArrayList<>();
+
+        try {
+            ResultSet result = DBController.executeQuery(query);
+            while (result.next()) {
+                Resource object = mapColumn(result);
+                resultList.add(object);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return resultList;
+    }
+
+    @Override
+    public boolean update(Resource object) {
+        String query = String.format("update %s set CATEGORY_ID = ?, NAME = ?, FILE_NAME = ?, DESCRIPTION = ?, `SIZE` = ? where id = ?", TABLE_NAME);
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement = putValues(statement, object);
+            statement.setInt(6, object.getId());
+            statement.executeUpdate();
+
+            return true;
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean delete(Resource object) {
+        String query = String.format("DELETE from %s where id = ?", TABLE_NAME);
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, object.getId());
+            statement.executeUpdate();
+
+            return true;
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public int getCount() {
+        String query = String.format("SELECT count(*) as record_count FROM %s", COLUMNS_STR, TABLE_NAME);
+
+        int count = 0;
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            if(resultSet.next()) {
+                count = resultSet.getInt("record_count");
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return count;
+    }
+
+    @Override
+    public boolean emptyTable() {
+        String query = "truncate table " + TABLE_NAME;
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.executeUpdate();
+
+            return true;
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public Resource mapColumn(ResultSet result) throws SQLException {
+        return new Resource(
+                result.getInt("ID"),
+                new ResourceCategory(result.getInt("CATEGORY_ID"), ""),
+                result.getString("NAME"),
+                result.getString("FILE_NAME"),
+                result.getString("DESCRIPTION"),
+                result.getDouble("SIZE")
+        );
+    }
+
+    @Override
+    public PreparedStatement putValues(PreparedStatement statement, Resource object) throws SQLException {
+        statement.setInt(1, object.getCategory().getId());
+        statement.setString(2, object.getName());
+        statement.setString(3, object.getFileName());
+        statement.setString(4, object.getDescription());
+        statement.setDouble(5, object.getSize());
+        return statement;
+    }
+}
---
+++ src/main/java/net/golbarg/kankor/db/TableResourceCategory.java
@@ -0,0 +1,168 @@
+package net.golbarg.kankor.db;
+
+import net.golbarg.kankor.model.Resource;
+import net.golbarg.kankor.model.ResourceCategory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class TableResourceCategory implements CRUDHandler<ResourceCategory> {
+    public static final String TABLE_NAME = "EMAILS";
+    public static final String [] COLUMNS = {"ID", "TITLE"};
+    public static final String COLUMNS_STR = "ID, TITLE";
+
+
+    @Override
+    public boolean create(ResourceCategory object) {
+        String query = String.format("insert into %s (TITLE) values (?)", TABLE_NAME);
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement = putValues(statement, object);
+            statement.executeUpdate();
+
+            return true;
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public ResourceCategory findById(int id) {
+        String query = String.format("SELECT %s FROM %s where id = ?;", COLUMNS_STR, TABLE_NAME);
+
+        ResourceCategory object = null;
+
+        try {
+
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if(resultSet.next()) {
+                object = mapColumn(resultSet);
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return object;
+    }
+
+    @Override
+    public ArrayList<ResourceCategory> getAll() {
+        String query = String.format("SELECT %s FROM %s;", COLUMNS_STR, TABLE_NAME);
+
+        ArrayList<ResourceCategory> resultList = new ArrayList<>();
+
+        try {
+            ResultSet result = DBController.executeQuery(query);
+            while (result.next()) {
+                ResourceCategory object = mapColumn(result);
+                resultList.add(object);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return resultList;
+    }
+
+    @Override
+    public boolean update(ResourceCategory object) {
+        String query = String.format("update %s set TITLE = ? where id = ?", TABLE_NAME);
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement = putValues(statement, object);
+            statement.setInt(2, object.getId());
+            statement.executeUpdate();
+
+            return true;
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean delete(ResourceCategory object) {
+        String query = String.format("DELETE from %s where id = ?", TABLE_NAME);
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, object.getId());
+            statement.executeUpdate();
+
+            return true;
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public int getCount() {
+        String query = String.format("SELECT count(*) as record_count FROM %s", COLUMNS_STR, TABLE_NAME);
+
+        int count = 0;
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            if(resultSet.next()) {
+                count = resultSet.getInt("record_count");
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return count;
+    }
+
+    @Override
+    public boolean emptyTable() {
+        String query = "truncate table " + TABLE_NAME;
+
+        try {
+            Connection connection = DBController.getLocalConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.executeUpdate();
+
+            return true;
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public ResourceCategory mapColumn(ResultSet result) throws SQLException {
+        return new ResourceCategory(
+                result.getInt("ID"),
+                result.getString("TITLE")
+        );
+    }
+
+    @Override
+    public PreparedStatement putValues(PreparedStatement statement, ResourceCategory object) throws SQLException {
+        statement.setString(1, object.getTitle());
+        return statement;
+    }
+}

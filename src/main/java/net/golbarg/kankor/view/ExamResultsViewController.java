0dc85cbe043d75057cd1ab873c22980d780dbe02
Mustafa Poya <mustafapoyaabc@gmail.com>
2023-03-25 13:09:00 +0430

    create exam view

--- src/main/java/net/golbarg/kankor/view/ExamResultsViewController.java
+++ src/main/java/net/golbarg/kankor/view/ExamResultsViewController.java
@@ -8,11 +8,12 @@ import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import net.golbarg.kankor.model.KankorResult;
+import net.golbarg.kankor.model.UniversityFaculty;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class KankorResultViewController implements Initializable {
+public class ExamResultsViewController implements Initializable {
     @FXML
     private BorderPane root;
     @FXML
@@ -27,10 +28,24 @@ public class KankorResultViewController implements Initializable {
     private Button btnSearch;
     @FXML
     private ProgressBar progressBar;
+
     @FXML
-    private ScrollPane scrollPaneContent;
+    private TableView<UniversityFaculty> tableViewExamResults;
     @FXML
-    private VBox vbContentContainer;
+    private TableColumn<UniversityFaculty, String> columnNumber;
+    @FXML
+    private TableColumn<UniversityFaculty, String> columnFullName;
+    @FXML
+    private TableColumn<UniversityFaculty, String> columnSchool;
+    @FXML
+    private TableColumn<UniversityFaculty, String> columnProvince;
+    @FXML
+    private TableColumn<UniversityFaculty, String> columnScore;
+    @FXML
+    private TableColumn<UniversityFaculty, String> columnAcceptedField;
+    @FXML
+    private TableColumn<UniversityFaculty, String> columnExamDate;
+
 
     final String[] types = new String[] { "همه", "آی دی", "نام", "مکتب", "ولایت", };
     ObservableList<String> typeList = FXCollections.observableArrayList(types);
